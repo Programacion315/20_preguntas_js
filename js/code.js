0852504e5d629f309d15e9abe16a3b83577ba993
Programacion <jorgeluishuergo88@gmail.com>
2022-03-12 00:12:01 -0500

    Añadiendo solucion pregunta dos

--- js/code.js
+++ js/code.js
@@ -1,1 +1,25 @@
-alert("holis");
+const comparar = () => {
+
+    let txt_uno = document.getElementById('numero_uno').value;
+    let txt_dos = document.getElementById('numero_dos').value;
+    let res = document.getElementById('res');
+
+    let v = "";
+
+    if(txt_uno > txt_dos){
+        v = "mayor";
+    } 
+    else if(txt_uno < txt_dos){
+        v = "menor";
+    }
+    else{
+        v = "igual"
+    }
+
+    res.innerHTML += `El numero uno es ${v} que el numero dos`;
+}
+ 
+
+
+
+
